33f372bdfcf40b11a2a9de224a73bd2100b8d669
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-04-25 20:11:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   cell="   "
-  raw=[ cell+"|"]
+  separator= "\n-----------\n"
+  raw=[ cell+"|"+cell+"|"+cell+"|"]
   puts raw
 end
 #expected: "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+#Failure/Error: expect(output).to include("   |   ")
