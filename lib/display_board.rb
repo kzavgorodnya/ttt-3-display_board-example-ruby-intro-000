074db6b88aca5360d1267f7e557c12296618efd0
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-04-25 20:17:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,20 @@
 def display_board
   cell="   "
   separator= "\n-----------\n"
-  raw1=[ cell+"|"+cell+"|"+cell+"|"]
+  one_raw=[ cell+"|"+cell+"|"+cell]
+
+  raw1=one_raw
   raw2=separator
+  raw3=one_raw
+  raw4=separator
+  raw5=one_raw
 
   puts raw1
   puts raw2
+  puts raw3
+  puts raw4
+  puts raw5
+
 end
 #expected: "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 #Failure/Error: expect(output).to include("   |   ")
